0f4b476498e97a8687b605f6a9946bb5f35c63a1
Mike Wood <Mwood@softdocs.com>
2022-04-19 20:46:35 -0400

    make responsive

--- src/routes/tbd/index.js
+++ src/routes/tbd/index.js
@@ -4,7 +4,7 @@ import style from './style.css';
 const tbd = () => (
 	<div class={style.tbd}>
 		<div class={style.logo}>
-			<img src='./assets/icons/logo.PNG' height={480} width={480} />
+			<img src='./assets/icons/logo.PNG'/>
 		</div>
         <div class={style.msg}>
                 <h1>Coming Soon...</h1>
